2e24212c02151fbc288ab6e1d6bc4a301d9d2923
kirti18patel <kirti18patel@yahoo.com>
2021-08-29 22:07:49 -0400

    contact section added

--- react-portfolio/src/components/Contact/index.js
+++ react-portfolio/src/components/Contact/index.js
@@ -3,6 +3,15 @@ function Contact() {
   return (
     <section className="my-5">
         <h2>Contact Me</h2>
+        <p>I am intrested in web development opportunities. If you have other question or want to discuss more about, don't hesitate to reach me.</p>
+        <form>
+            <input placeholder="Name"></input>
+            <input placeholder="Email"></input>
+            <input placeholder="Subject"></input>
+            <textarea placeholder="Message"></textarea>
+            <button>Send Message</button>
+        </form>
+        <p>address</p>
     </section>
   );
 }
