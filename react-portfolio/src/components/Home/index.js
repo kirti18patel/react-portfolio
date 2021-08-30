bd477f793bdd7f42b24b42334c1f001fc4d5fd3d
kirti18patel <kirti18patel@yahoo.com>
2021-08-29 22:08:20 -0400

    home section added

--- react-portfolio/src/components/Home/index.js
+++ react-portfolio/src/components/Home/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 function Home() {
   return (
     <section className="my-5">
-      <h1>home</h1>
+      <h1>Web is fun!</h1>
+      <h1>Hi, I am Kirti, Full Stack Web developer. I write code and improve my web design and skills every day. I am honored to work with special people and looking forward to meet ambitious people ahead.
+      </h1>
+      <a href="mailto:kirti18patel@yahoo.com" >Contact Me!</a>
     </section>
   );
 }
