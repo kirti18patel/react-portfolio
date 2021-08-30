06fa45d9554149c4e9df38aa61693dfce8d713fe
kirti18patel <kirti18patel@yahoo.com>
2021-08-29 23:14:57 -0400

    navigatin styling completed

--- react-portfolio/src/components/Nav/index.js
+++ react-portfolio/src/components/Nav/index.js
@@ -6,8 +6,8 @@ function Nav(props) {
     const menu = ['About', 'Skills', 'Work', 'Contact']
   return (
 <nav>
-<div> <img src={logo} alt="logo" onClick={() => (props.setCurrentCategory('home'))}></img> </div>
-<div>
+<div className="logo"> <img src={logo} alt="logo" onClick={() => (props.setCurrentCategory('home'))}></img> </div>
+<div className= "menu">
   <ul>
       {
           menu.map(item => (
@@ -22,7 +22,7 @@ function Nav(props) {
   </ul>
 </div>
 <div>Social media icons</div>
-<a href={resume} download="resume-KirtiPatel"> <i className="fa fa-download" aria-hidden="true"></i>  </a>
+<h1><a href={resume} download="resume-KirtiPatel" className="tooltip"> <i className="fa fa-download" aria-hidden="true"></i><span class="tooltiptext">Download my resume</span></a></h1>
 </nav>
   );
 }
