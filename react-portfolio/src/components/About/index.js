5d3208c40b892f177ea69b9007300758610f085b
kirti18patel <kirti18patel@yahoo.com>
2021-08-29 22:06:56 -0400

    about section added

--- react-portfolio/src/components/About/index.js
+++ react-portfolio/src/components/About/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 function About() {
   return (
-    <section className="about">
-      <h1>about</h1>
+    <section className="my-5">
+        <h2>Hey there, lets know a bit more about me, </h2>
+<p>I am a passionate full stack developer, located in Canada. I have a serious passion in web development and creating 
+intuitive, dynamic user experiences. 
+
+I have 1.5 years of experience as a developer and love working with React and other frameworks.
+I am confident using the MERN Full-Stack and pursue good experience and knowledge in web technologies. 
+
+Effective at combining creativity and problem solving to develop user-friendly applications. 
+
+Strengths include creativity, teamwork, and trustworthiness. Well-organised person, problem solver, independent employee with high attention to detail and life-long dedication to learning. Love to do painting and write songs in my leisure time.
+
+Love go explore beaty of nature, preferably hiking, for my vacation.
+
+Interested in the entire web spectrum and working on ambitious projects with positive people.</p>
+<p>Stay bold &
+
+Have an outstanding Saturday</p>
     </section>
   );
 }
